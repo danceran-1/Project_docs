4dd6571c2958eb1c7faa2be2e7455a039f4ff674
Alexey <alexey.hrulyow@yandex.ru>
2025-07-24 22:04:07 +0300

    Тест

---
+++ site/main/urls.py
@@ -0,0 +1,17 @@
+# Ссылки на странички. Каждую новую страницу добавлять сюда
+from django.contrib import admin
+from django.urls import path, include
+from  . import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('',views.index),
+    path('about/', views.about, name='about'),
+    path('success/', views.success, name='success'),
+    path('success1/', views.success, name='success1'),
+    path('media/', views.media, name='media_view'),
+
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
