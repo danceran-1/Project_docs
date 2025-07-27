213c4cb78e8089bdbbc82f9ee838b797ab3fa034
danceran <danceran@student.21-school.ru>
2025-07-27 18:56:17 +0300

    password_hashed

--- site/create.sql
+++ site/create.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS roles;
 CREATE TABLE roles(
     id SERIAL PRIMARY KEY,
     role_name VARCHAR(10) UNIQUE NOT NULL
@@ -12,31 +13,33 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL UNIQUE,
-    user_password VARCHAR(50) NOT NULL,
+    user_password  VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
 INSERT INTO users (name, user_password) VALUES 
-('Ольга', '123'),
-('Дмитрий', '111'),
-('Елена', '222'),
-('Сергей', '333'),
-('Анна', '000');
+('Леха','$2b$12$Q/BWj6KpvXeegUJbC79m8Oh3cN7ugiqTr8Rbt/LpNkK3V4/jhi97G'),
+('Ольга', '$2b$12$EicQ7HZ.B8W6J7X3Y2nYp.9gT7U1wRjJq3kYlLmNpQsWvX1V2sZbG'),
+('Дмитрий', '$2b$12$N9qo8uLOickgx2ZMRZoMy.Mrq4L9n7M3B6YFJYzQjFjTfYV6WY1Oe'),
+('Елена', '$2b$12$3mR5W7vTcR1H9qKJ8bL0NuYjXz2V4sDr9wA1B2C3D4E5F6G7H8I9J'),
+('Сергей', '$2b$12$7yH8J9K0L1M2N3O4P5Q6R.SaTbUcVdWeXfYgZhAiBjCkDlEmFnGoH'),
+('Анна', '$2b$12$1A2B3C4D5E6F7G8H9I0J.KlMnOpQrStUvWxYzZaBcDeEfFgGhHiJkL');
 
-
+DROP TABLE IF EXISTS loging_password;
 CREATE TABLE loging_password(
     id SERIAL PRIMARY KEY,
     role_id INT REFERENCES roles(id),
     loggin VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL
+    password VARCHAR(255) NOT NULL
 )
 
 INSERT INTO loging_password(role_id, loggin, password) 
 VALUES 
-(1, 'admin', 'Iadmin'),
-(2, 'manager', 'Imanager'),
-(3, 'user', 'Iuser'),
-(4, 'guest', '123');
+(1, 'admin', '$2b$12$rvsio4NfVE9vXiMPT7KO5.fNr01NyAhTY.NGF7YfSuHDPoHEqzn7u'),
+(2, 'manager', '$2b$12$LjaQPEGZGqXsMY.z00jm9eRmAcMIgA2Wo6tiNayDt3LmiJXPxtw4i'),
+(3, 'user', '$2b$12$qcXYWK6duTdCOlsLWBajGeo0oO0i.EO6g5e1GsIEhWR6mkVEqnMnK');
+
+#индексы
 
 CREATE INDEX idx_users_username ON users(name);
-CREATE INDEX idx_users_role  ON users(role_id);
+CREATE INDEX idx_users_role  ON users(user_password);
--- site/main/views.py
+++ site/main/views.py
@@ -5,30 +5,32 @@ from .forms import CriterionForm
 from django.conf import settings
 from django.db import connection
 import os
+import bcrypt 
 
 
 def index(request):
     return render(request, 'main/index.html')
 
 
-def special_users(request,password,user_id):
-    
-    with connection.cursor() as cursor:
-        cursor.execute(
-            "SELECT loggin FROM loging_password WHERE password = %s AND id = %s",
-            [password, user_id]
-            )
-        
-        auth_data = cursor.fetchone()
+def special_users(request,password,loggin,spesial_password):
 
-        if not auth_data:
-            error_message = 'Неверный пароль для спецпользователя'
-            return render(request, 'main/about.html',
-                    {'form': RegistrationForm(), 'error_message': error_message})
+        try:
+            hash_bytes = spesial_password.encode('utf-8')
+            
+            if bcrypt.checkpw(password.encode('utf-8'), hash_bytes):
+                success_message = f"Авторизация для {loggin} выполнена успешно"
+                request.session['is_special'] = True
+                return redirect('success')
 
-        name = auth_data[0]
-        request.session['is_special'] = True
-        return redirect('success')
+            else:
+                error_message = 'Неверный пароль'
+                return render(request, 'main/about.html',
+                        {'form': RegistrationForm(), 'error_message': error_message})
+            
+        except Exception as e:
+                error_message = 'Ошибка аутентификации'
+                return render(request, 'main/about.html',
+                           {'form': RegistrationForm(), 'error_message': error_message})
 
 
 def about(request):
@@ -39,43 +41,28 @@ def about(request):
         with connection.cursor() as cursor:
             # сначала ищем спец пользователей
             cursor.execute(
-                "SELECT id FROM loging_password WHERE loggin = %s",
+                "SELECT loggin,password FROM loging_password WHERE loggin = %s",
                 [username]
             )
             special = cursor.fetchone()
 
             if special:
-                return special_users(request, password, special[0])
+                return special_users(request, password,special[0],special[1])
 
             # тут обычных
             cursor.execute(
-                "SELECT id FROM users WHERE name = %s",
+                "SELECT user_password,name FROM users WHERE name = %s",
                 [username]
             )
             user_data = cursor.fetchone()
 
-            if not user_data:
+            if user_data:
+                return special_users(request, password,user_data[1],user_data[0])
+
+            else:
                 error_message = 'Пользователь не найден'
                 return render(request, 'main/about.html', 
                            {'form': RegistrationForm(), 'error_message': error_message})
-
-            user_id = user_data[0]
-
-            cursor.execute(
-                "SELECT name FROM users WHERE user_password = %s AND id = %s",
-                [password, user_id]
-            )
-            auth_data = cursor.fetchone()
-
-            if not auth_data:
-                error_message = 'Неверный пароль'
-                return render(request, 'main/about.html',
-                           {'form': RegistrationForm(), 'error_message': error_message})
-
-            name = auth_data[0]
-            request.session['user_id'] = user_id
-            success_message = f"Авторизация для {name} выполнена успешно"
-            return redirect('success')
 
     else:
         return render(request, 'main/about.html', {'form': RegistrationForm()})
