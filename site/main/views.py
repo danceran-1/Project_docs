1bdcd7cf2b4dcd4f61dadc78cb4eea5c1e16a1af
danceran <danceran@student.21-school.ru>
2025-08-06 14:07:11 +0300

    save_doc

--- site/main/views.py
+++ site/main/views.py
@@ -18,6 +18,11 @@ from django.http import JsonResponse
 from .models import City
 from docxtpl import DocxTemplate
 
+from django.http import HttpResponse
+from docxtpl import DocxTemplate
+from io import BytesIO
+from django.db import connection
+
 def index(request):
     return render(request, 'main/index.html')
 
@@ -242,9 +247,28 @@ def city_autocomplete(request):
         results = []
     return JsonResponse(results, safe=False)
 
+def download_doc(user_id,request,context,path):
 
-def generate_doc(user_id):
+    doc = DocxTemplate(path)
+    doc.render(context)
+
+    file_stream = BytesIO()
+    doc.save(file_stream)
+    file_stream.seek(0)
+
+    response = HttpResponse(
+        file_stream.getvalue(),
+        content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    )
+    response['Content-Disposition'] = 'attachment; filename="spravka.docx"'
+    return response
+
+
+def generate_doc(user_id,request):
     
+
+    path = "main/templates/documents/example.docx"
+
     with connection.cursor() as cursor:
         cursor.execute(
             "SELECT * FROM personal_data WHERE user_id = %s",
@@ -253,21 +277,23 @@ def generate_doc(user_id):
         is_data = cursor.fetchone()
 
         if not is_data:
-            messages.error(is_data, "Нет данных для генерации документа.")
+            messages.success(request, "Нет данных для генерации документа.")
 
         else:
-            name, surname, last_name, dob, city = is_data
+            name = is_data[1]
+            surname = is_data[2] 
+            last_name = is_data[3] 
+            dob = is_data[4] 
+            city = is_data[5] 
+           
 
             context = {
                 'ФИО': f'{name} {surname} {last_name}',
-                'Город проживания': city,
-                'Дата рождения': dob.strftime('%d.%m.%Y')
+                'Город_проживания': city,
+                'Дата_рождения': dob.strftime('%d.%m.%Y')
             }
-
-    doc = DocxTemplate("templates/documents/example.docx")
-    doc.render(context)
-    doc.save("templates/out_doc/spravka_ivanov.docx")
-
+            messages.success(request, "Документ успешно создан.")
+            return download_doc(user_id,request,context,path)
 
 
 def success(request, username,user_id):
@@ -286,33 +312,33 @@ def success(request, username,user_id):
 
         if 'generate_doc_btn' in request.POST:
 
-            generate_doc(user_id)
+            return generate_doc(user_id, request)
 
-        elif "save-btn" in request.POST:
+        
 
-            last_name = request.POST.get('last_name', '').strip()
-            first_name = request.POST.get('first_name', '').strip()
-            middle_name = request.POST.get('middle_name', '').strip()
-            birth_date = request.POST.get('birth_date', '').strip()
-            city = request.POST.get('city', '').strip()
+        last_name = request.POST.get('last_name', '').strip()
+        first_name = request.POST.get('first_name', '').strip()
+        middle_name = request.POST.get('middle_name', '').strip()
+        birth_date = request.POST.get('birth_date', '').strip()
+        city = request.POST.get('city', '').strip()
 
             
 
-            with connection.cursor() as cursor:
+        with connection.cursor() as cursor:
                 cursor.execute(
                     "SELECT * FROM personal_data WHERE user_id = %s",
                     [user_id]
                 )
                 is_data = cursor.fetchone()
 
-            if is_data:
+        if is_data:
                 with connection.cursor() as cursor:
                     cursor.execute("""
                         UPDATE personal_data SET name = %s, surname = %s, last_name = %s, dob = %s, sity = %s 
                         WHERE user_id = %s""", [first_name, last_name, middle_name, birth_date, city, user_id])
 
                 
-            else:
+        else:
                 with connection.cursor() as cursor:
                     cursor.execute(
                         "INSERT INTO personal_data (user_id, name,surname,last_name,dob,sity) VALUES (%s, %s,%s,%s,%s,%s)",
